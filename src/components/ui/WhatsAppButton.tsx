761a768b7bcf0f54c47276757bbd2c71e254ed6f
Carlos <mtv-astro@proton.me>
2025-07-25 17:04:12 -0300

    ajustes finais

--- src/components/ui/WhatsAppButton.tsx
+++ src/components/ui/WhatsAppButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/5548996946958"
+      href="https://wa.me/55489929-3876"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
